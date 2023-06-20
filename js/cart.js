81ac1965f9f7bbdba2096957c9dd87cd6b350a0c
tomyslon <tomyslon@gmail.com>
2023-06-20 20:03:52 -0300

    m

--- js/cart.js
+++ js/cart.js
@@ -1,34 +1,58 @@
 let list = document.querySelectorAll('.list .item');
 list.forEach(item => {
-    item.addEventListener('click', function(event){
-        if(event.target.classList.contains('add')){
-            var itemNew = item.cloneNode(true);
-            let checkIsset  = false;
+  item.addEventListener('click', function(event) {
+    if (event.target.classList.contains('add')) {
+      let itemNew = item.cloneNode(true);
+      let itemId = itemNew.getAttribute('data-key');
+      let cartItem = document.querySelector(`.cart .item[data-key="${itemId}"]`);
+      if (cartItem) {
+        let countInput = cartItem.querySelector('.count');
+        let count = parseInt(countInput.value);
+        countInput.value = count + 1;
+      } else {
+        document.querySelector('.listCart').appendChild(itemNew);
+      }
+      updateTotal(); // Actualizar el total al agregar un producto
+    }
+  })
+});
 
-            let listCart = document.querySelectorAll('.cart .item');
-            listCart.forEach(cart =>{
-                if(cart.getAttribute('data-key') == itemNew.getAttribute('data-key')){
-                    checkIsset = true;
-                    cart.classList.add('danger');
-                    setTimeout(function(){
-                        cart.classList.remove('danger');
-                    },1000)
-                }
-            })
-            if(checkIsset == false){
-                document.querySelector('.listCart').appendChild(itemNew);
-            }
-
-        }
-    })
-})
-
-function Remove($key){
-    let listCart = document.querySelectorAll('.cart .item');
-    listCart.forEach(item => {
-        if(item.getAttribute('data-key') == $key){
-            item.remove();
-            return;
-        }
-    })
+function Remove(key) {
+  let listCart = document.querySelectorAll('.cart .item');
+  listCart.forEach(item => {
+    if (item.getAttribute('data-key') == key) {
+      item.remove();
+      updateTotal();
+      return;
+    }
+  })
 }
+
+function updateTotal() {
+  let totalPrice = 0;
+  let cartItems = document.querySelectorAll('.cart .item');
+  
+  cartItems.forEach(item => {
+    let priceElement = item.querySelector('.price');
+    let price = parseFloat(priceElement.dataset.price); // Usar dataset para obtener el precio
+    let countInput = item.querySelector('.count');
+    let count = parseInt(countInput.value);
+    totalPrice += price * count;
+  });
+
+  let totalAmountElement = document.querySelector('.cart .total-amount');
+  totalAmountElement.textContent = `$${totalPrice.toFixed(2)}`;
+}
+
+let buyButton = document.querySelector('.cart .buy');
+buyButton.addEventListener('click', function() {
+  alert('¡Gracias por tu compra!');
+});
+
+// Desactivar clic en elementos del carrito
+let cartItems = document.querySelectorAll('.cart .item');
+cartItems.forEach(item => {
+  item.addEventListener('click', function(event) {
+    event.stopPropagation();
+  });
+});
